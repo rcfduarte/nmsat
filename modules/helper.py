c5c1f422f158125f1442c6866e359ee9b6bfeeb2
Barna Zajzon <b.zajzon@fz-juelich.de>
2016-12-08 19:47:00 +0100

    Removing signals package from master branch.

--- modules/helper.py
+++
@@ -1,8 +0,0 @@
-
-def isiterable(x):
-	"""
-	Verify if input is iterable (list, dictionary, array...)
-	:param x: input
-	:return: boolean
-	"""
-	return hasattr(x, '__iter__') and not isinstance(x, basestring)
