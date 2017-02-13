f62f3e5ac929ab723ce76813f3d89fbfc1c3453b
Barna Zajzon <b.zajzon@fz-juelich.de>
2017-02-13 11:35:29 +0100

    Fixed progress bar for Python3

--- modules/visualization.py
+++ modules/visualization.py
@@ -2728,7 +2728,7 @@ def progress_bar(progress):
 	assert (type(progress) == float) and (progress >= 0.) and (progress <= 1.), progressConditionStr
 	length = 50
 	filled = int(round(length * progress))
-	print("|" + "=" * filled + " " * (length - filled) + "|\r", end=' ')
+	sys.stdout.write("\r|" + "=" * filled + " " * (length - filled) + "|")
 	sys.stdout.flush()
 
 
